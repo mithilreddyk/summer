c75878e17a5257bd5e4ee2fe4305cc395c374942
“mithilreddyk” <mithilreddykai@gmail.com>
2024-06-13 02:13:24 -0400

    commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const mongoose =  require("mongoose");
 const path = require('path');
 
 app.use(express.json());
@@ -11,6 +13,9 @@ res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 next();
 });
 
+mongoose.connect(process.env.dbURL)
+.then(console.log("DB Connected !!"))
+.catch(error => console.log(error));
 
 app. use(express.static(__dirname+"/public"));
 app.get('/', (req, res) => res.sendFile(path.join(_dirname, '/public', 'index.html')));
