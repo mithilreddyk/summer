5352245533663add6e886bd409860f7a8b6c8543
mithilreddyk <43516251+mithilreddyk@users.noreply.github.com>
2024-06-15 16:38:28 -0400

    Update index.js 2

--- index.js
+++ index.js
@@ -33,4 +33,4 @@ app.use(function(req,res,next){
 app.use('/user',userRoutes);
 app.use('/post',postRoutes);
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}!`)); 
